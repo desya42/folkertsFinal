23b52a71f14f8c893d447830120482d286146c8f
desya42 <59778932+desya42@users.noreply.github.com>
2020-05-04 00:32:33 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -23,5 +23,5 @@ xhr.onload = function() {
   //}
 };
 
-xhr.open('GET', './data/finalSyllabus.json', true);        // Prepare the request
+xhr.open('GET', 'https://desya42.github.io/folkertsFinal/data/finalSyllabus.json', true);        // Prepare the request
 xhr.send(null);                                 // Send the request
